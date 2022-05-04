750317db29fa65e007b9a9748dfa6264755f08d8
sriram <36560070+chintasriram@users.noreply.github.com>
2022-05-04 15:48:07 -0700

    Add files via upload

---
+++ backend/middleware/auth.js
@@ -0,0 +1,18 @@
+const jwt = require("jsonwebtoken");
+const config = require("config");
+
+const verifyToken = (req, res, next) => {
+  const token = req.body.token || req.query.token || req.headers['authorization'].split(" ")[1];
+  if (!token) {
+    return res.status(403).send("A token is required for authentication");
+  }
+  try {
+    const decoded = jwt.verify(token, config.get("jwtPrivateKey"));
+    req.user = decoded;
+  } catch (err) {
+    return res.status(401).send("Invalid Token");
+  }
+  return next();
+};
+
+module.exports = verifyToken;
