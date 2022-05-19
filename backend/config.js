9dec12d9043f197e46ac11eb986544f3498ebb20
sandeepbirudukota <98917512+sandeepbirudukota@users.noreply.github.com>
2022-05-18 21:41:43 -0700

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     mongoURI:"mongodb+srv://admin:etsyadmin@etsy-cluster.d0crh.mongodb.net/etsyDatabase?retryWrites=true&w=majority",
     secret: "cmpe273_secret_key",
-    frontEnd: "http://localhost:3000",
-    //frontEnd: "http://13.56.79.52:3000"
+    //frontEnd: "http://localhost:3000",
+    frontEnd: "http://54.177.238.130:3000"
 
 }
