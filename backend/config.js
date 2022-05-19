ff937e033aeae4b32cda4e05ddc5244b6446bd93
sriram <36560070+chintasriram@users.noreply.github.com>
2022-05-18 22:06:08 -0700

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -2,6 +2,6 @@ module.exports = {
     mongoURI:"mongodb+srv://admin:etsyadmin@etsy-cluster.d0crh.mongodb.net/etsyDatabase?retryWrites=true&w=majority",
     secret: "cmpe273_secret_key",
     //frontEnd: "http://localhost:3000",
-    frontEnd: "http://54.177.238.130:3000"
+    frontEnd: "https://54.183.228.142:3000"
 
 }
